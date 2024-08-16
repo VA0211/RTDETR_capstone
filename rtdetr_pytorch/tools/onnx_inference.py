3159f2a55259206c1b155b957777b0e7ed28c290
Your Name <you@example.com>
2024-08-17 00:13:33 +0700

    check if output is none

--- rtdetr_pytorch/tools/onnx_inference.py
+++ rtdetr_pytorch/tools/onnx_inference.py
@@ -79,13 +79,14 @@ def main(args):
             box = boxes[i][scr > thrh]
 
             # Map the category ID to the class name
-            print('Model predict:', lab[i])
-            category_id = coco_dataset.mscoco_label2category[lab[i]]
-            class_name = coco_dataset.mscoco_category2name[category_id]
+            # print('Model predict:', lab[i])
+            if lab[i].size != 0:
+                category_id = coco_dataset.mscoco_label2category[lab[i]]
+                class_name = coco_dataset.mscoco_category2name[category_id]
 
-            for b in box:
-                draw.rectangle(list(b), outline='red')
-                draw.text((b[0], b[1]), text=str(class_name), fill='yellow')
+                for b in box:
+                    draw.rectangle(list(b), outline='red')
+                    draw.text((b[0], b[1]), text=str(class_name), fill='yellow')
                 
         file_dir = Path(img_path).parent.parent / 'onnx_output'
         createDirectory(file_dir)
