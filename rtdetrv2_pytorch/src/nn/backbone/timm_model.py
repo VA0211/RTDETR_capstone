a1c58dabe91343d7153d856d9a742671550e3f8a
Your Name <you@example.com>
2024-08-18 09:35:33 +0700

    try TimmModel

--- rtdetrv2_pytorch/src/nn/backbone/timm_model.py
+++ rtdetrv2_pytorch/src/nn/backbone/timm_model.py
@@ -9,6 +9,7 @@ from torchvision.models.feature_extraction import get_graph_node_names, create_f
 from .utils import IntermediateLayerGetter
 from ...core import register
 
+__all__ = ['TimmModel']
 
 @register()
 class TimmModel(torch.nn.Module):
